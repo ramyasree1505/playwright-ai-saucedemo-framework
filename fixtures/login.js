868a575d94acdb077e0bb21f867c49a48888529b
ramyasree1505 <ramyasreeslr@gmail.com>
2026-04-06 14:50:06 +0530

    Upgraded fixtures and usage

--- fixtures/login.js
+++ fixtures/login.js
@@ -1,13 +1,20 @@
 const base = require('@playwright/test');
 const LoginPage = require('../pages/LoginPage');
+const { users } = require('../testData/userData');
 
 exports.test = base.test.extend({
-// Define a fixture for the login page
+
+  // Base fixture for login page
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-
     await use(loginPage);
+  },
+
+  // Reuse loginPage here to perform login and provide loggedInPage for tests
+  loggedInPage: async ({ loginPage }, use) => {
+    await loginPage.login(users.standard.username,users.standard.password);
+    await use(loginPage.page);
   }
 
 });
